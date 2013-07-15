4483eb8c6b9b2156bd4c4eefcdc70088b559d03a
David Nimmo <dnimmo@gmail.com>
2013-07-15 12:34:23 +0000

    Added templates

--- djangotuts/hackernews/hackernews/settings.py
+++ djangotuts/hackernews/hackernews/settings.py
@@ -1,4 +1,7 @@
 # Django settings for hackernews project.
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,7 +114,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+    os.path.join(PROJECT_PATH, 'templates'),
+)  
 
 INSTALLED_APPS = (
     'django.contrib.auth',
